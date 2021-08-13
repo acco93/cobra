0d198953480944cc0022054e53523f1c8b7a05e7
cava <francescocava95@gmail.com>
2021-08-13 15:12:47 +0200

    Time based simulated annealing

--- include/cobra/SimulatedAnnealing.hpp
+++ include/cobra/SimulatedAnnealing.hpp
@@ -48,6 +48,42 @@ class SimulatedAnnealing {
 
 };
 
+class TimeBasedSimulatedAnnealing {
+
+ public:
+    TimeBasedSimulatedAnnealing(float initial_temperature_, float final_temperature_, std::mt19937 &rand_engine_, int time)
+        : rand_engine(rand_engine_), uniform_dist(0.0f, 1.0f) {
+
+        initial_temperature = initial_temperature_;
+        final_temperature = final_temperature_;
+        temp_ratio = final_temperature / initial_temperature;
+        period = time;
+    }
+
+
+    bool accept(const cobra::Solution &solution, const cobra::Solution &neighbor, double elapsed_time) {
+        const double temperature = initial_temperature * std::pow(temp_ratio, elapsed_time / static_cast<float>(period));
+        return neighbor.get_cost() < solution.get_cost() - temperature * std::log(uniform_dist(rand_engine));
+    }
+
+    float get_temperature(double elapsed_time) const { return initial_temperature * std::pow(temp_ratio, elapsed_time / static_cast<float>(period)); }
+
+    void decrease_temperature() { }
+
+ private:
+
+    float initial_temperature;
+    float final_temperature;
+    float temp_ratio;
+    int period;
+
+    std::mt19937 &rand_engine;
+    std::uniform_real_distribution<float> uniform_dist;
+
+    float factor;
+
+};
+
 }
 
 #endif //COBRA_SIMULATEDANNEALING_HPP
